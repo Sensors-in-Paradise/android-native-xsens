623b1880b6ad31d305c65f9fa4a5b088b33601a6
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-08 18:54:56 +0100

    Added list for displaying XSENS devices. No real functionality yet

--- app/src/main/java/sensors_in_paradise/xsens/MainActivity.kt
+++ app/src/main/java/sensors_in_paradise/xsens/MainActivity.kt
@@ -1,14 +1,18 @@
 package sensors_in_paradise.xsens
 
+import android.Manifest
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.le.ScanSettings
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.xsens.dot.android.sdk.XsensDotSdk
 import com.xsens.dot.android.sdk.events.XsensDotData
@@ -16,12 +20,13 @@ import com.xsens.dot.android.sdk.interfaces.XsensDotDeviceCallback
 import com.xsens.dot.android.sdk.interfaces.XsensDotScannerCallback
 import com.xsens.dot.android.sdk.models.FilterProfileInfo
 import com.xsens.dot.android.sdk.utils.XsensDotScanner
+import java.security.Permission
 import java.util.*
 
 class MainActivity : AppCompatActivity(), XsensDotDeviceCallback, XsensDotScannerCallback {
     private lateinit var tv: TextView
     private lateinit var rv: RecyclerView
-
+    private lateinit var sensorAdapter: SensorAdapter
     private val scannedDevices = ArrayList<BluetoothDevice>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,18 +36,33 @@ class MainActivity : AppCompatActivity(), XsensDotDeviceCallback, XsensDotScanne
         XsensDotSdk.setDebugEnabled(true)
         initXsScanner()
         val scanStarted = mXsScanner!!.startScan()
-        Log.println(Log.INFO, "XSENS", "")
+
         tv.text = "Scan started: $scanStarted"
 
         val intent = Intent(this, CaptureActivity::class.java)
-
         findViewById<Button>(R.id.btn_continue_activity_main).setOnClickListener {
             startActivity(intent)
         }
 
 
-    }
 
+        sensorAdapter = SensorAdapter(scannedDevices)
+        rv.adapter = sensorAdapter
+
+
+
+
+
+        var permissions = isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)
+        permissions =  permissions and isPermissionGranted(Manifest.permission.READ_EXTERNAL_STORAGE)
+        permissions =  permissions and isPermissionGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        permissions =  permissions and isPermissionGranted(Manifest.permission.ACCESS_COARSE_LOCATION)
+        Toast.makeText(this, "Necessary permissions granted: $permissions",Toast.LENGTH_LONG).show()
+
+    }
+    private fun isPermissionGranted(permission: String): Boolean {
+        return (ContextCompat.checkSelfPermission(this, permission) ==PackageManager.PERMISSION_GRANTED)
+    }
     private var mXsScanner: XsensDotScanner? = null
     private fun initXsScanner() {
         mXsScanner = XsensDotScanner(this, this)
@@ -64,14 +84,15 @@ class MainActivity : AppCompatActivity(), XsensDotDeviceCallback, XsensDotScanne
     override fun onXsensDotGetFilterProfileInfo(s: String, arrayList: ArrayList<FilterProfileInfo>) {}
     override fun onSyncStatusUpdate(s: String, b: Boolean) {}
     override fun onXsensDotScanned(device: BluetoothDevice, i: Int) {
+
         if(!scannedDevices.contains(device)){
             scannedDevices.add(device)
-            val name = device.name
-            val address = device.address
 
-            tv.visibility = View.INVISIBLE;
+            sensorAdapter.notifyItemInserted(scannedDevices.size-1)
+            //Toast.makeText(this, "Device discovered",Toast.LENGTH_LONG).show()
 
         }
+        tv.visibility = View.INVISIBLE;
 
     }
 }
---
+++ app/src/main/java/sensors_in_paradise/xsens/SensorAdapter.kt
@@ -0,0 +1,53 @@
+package sensors_in_paradise.xsens
+
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import androidx.recyclerview.widget.RecyclerView
+
+class SensorAdapter(private val devices: ArrayList<BluetoothDevice>) :
+    RecyclerView.Adapter<SensorAdapter.ViewHolder>() {
+
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textView = view.findViewById(R.id.tv_name_sensorDevice)
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.sensor_device, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.textView.text = devices[position].name +" "+ devices[position].address
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount(): Int {
+        return devices.size
+    }
+
+}
