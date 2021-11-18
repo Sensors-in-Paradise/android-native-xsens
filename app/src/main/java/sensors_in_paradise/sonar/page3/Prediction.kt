4b02c0da84eb0d0ff34bb1e471e47ed6db42500d
Tobias Fiedler <tfiedlerdevelopment@gmail.com>
2021-11-18 15:02:59 +0100

    Fix #16 Fix #17

---
+++ app/src/main/java/sensors_in_paradise/sonar/page3/Prediction.kt
@@ -0,0 +1,5 @@
+package sensors_in_paradise.sonar.page3
+
+class Prediction(var title: String, var content:String) {
+
+}
---
+++ app/src/main/java/sensors_in_paradise/sonar/page3/PredictionsAdapter.kt
@@ -0,0 +1,59 @@
+package sensors_in_paradise.sonar.page3
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import android.widget.ViewFlipper
+import androidx.recyclerview.widget.RecyclerView
+import com.xsens.dot.android.sdk.models.XsensDotDevice
+import sensors_in_paradise.sonar.R
+
+class PredictionsAdapter(
+    private val predictions: ArrayList<Prediction>,
+) :
+    RecyclerView.Adapter<PredictionsAdapter.ViewHolder>() {
+
+    /**
+     * Provide a reference to the type of views that you are using
+     * (custom ViewHolder).
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: TextView = view.findViewById(R.id.tv_title_prediction)
+        val detailsTextView: TextView = view.findViewById(R.id.tv_content_prediction)
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.prediction, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+
+       val prediction = predictions[position]
+        viewHolder.titleTextView.text = prediction.title
+        viewHolder.detailsTextView.text = prediction.content
+        Log.d("ADAPTER", "222Bind ViewHolder: "+position)
+        println("Binding view holder PREDICTION")
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount(): Int {
+        println("SIZE PREDICTION: "+predictions.size)
+        Log.d("ADAPTER", "SIZE PREDICTION: "+predictions.size)
+        return predictions.size
+
+    }
+
+}
