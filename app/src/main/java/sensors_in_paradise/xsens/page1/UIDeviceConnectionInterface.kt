b94898838062c2d09114af4e1a8e0a894234504c
treyfel <58570309+treyfel@users.noreply.github.com>
2021-11-18 11:25:20 +0100

    deleted unnecessary files and changed imports

--- app/src/main/java/sensors_in_paradise/xsens/page1/UIDeviceConnectionInterface.kt
+++
@@ -1,7 +0,0 @@
-package sensors_in_paradise.xsens.page1
-
-import com.xsens.dot.android.sdk.models.XsensDotDevice
-
-interface UIDeviceConnectionInterface {
-    fun onConnectionUpdateRequested(device: XsensDotDevice, wantsConnection: Boolean)
-}
