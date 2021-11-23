39f3238cd15fe43ee5dda867ec270f8897d63ea8
tfiedlerdev <tfiedlerdevelopment@gmail.com>
2021-11-23 14:38:20 +0100

    Fixes #40

--- app/src/main/java/sensors_in_paradise/sonar/PageInterface.kt
+++ app/src/main/java/sensors_in_paradise/sonar/PageInterface.kt
@@ -1,8 +1,11 @@
 package sensors_in_paradise.sonar
 
 import android.app.Activity
+import androidx.annotation.UiThread
 
 interface PageInterface {
+    @UiThread
     fun activityCreated(activity: Activity)
+    @UiThread
     fun activityResumed()
 }
