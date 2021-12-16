ea0c18a081d3cdeb6596060dacc4bc4083b77218
treyfel <58570309+treyfel@users.noreply.github.com>
2021-12-16 11:37:02 +0100

    2nd try

--- app/src/main/java/sensors_in_paradise/sonar/page2/RecordingHandler.kt
+++ app/src/main/java/sensors_in_paradise/sonar/page2/RecordingHandler.kt
@@ -1,89 +1,240 @@
 package sensors_in_paradise.sonar.page2
 
-import android.util.Log
+import android.app.Activity
+import android.content.Context
+import android.widget.Toast
 import com.xsens.dot.android.sdk.events.XsensDotData
 import com.xsens.dot.android.sdk.interfaces.XsensDotRecordingCallback
+import com.xsens.dot.android.sdk.models.XsensDotDevice
 import com.xsens.dot.android.sdk.models.XsensDotRecordingFileInfo
 import com.xsens.dot.android.sdk.models.XsensDotRecordingState
 import com.xsens.dot.android.sdk.recording.XsensDotRecordingManager
-import java.util.*
+import sensors_in_paradise.sonar.XSENSArrayList
 
-class RecordingHandler(private val callback: RecordingInterface) : XsensDotRecordingCallback {
-    override fun onXsensDotRecordingNotification(address: String?, isEnabled: Boolean) {
-        if (isEnabled) {
-            callback.requestFlashInfo()
+class RecordingHandler(
+    val context: Context,
+    private val devices: XSENSArrayList,
+    private val callback: RecordingInterface
+) : XsensDotRecordingCallback {
+    private val xsRecorders: ArrayList<RecordingManager> = ArrayList()
+    private lateinit var activity: Activity
+
+    init {
+        createLoggers()
+        enableNotifications()
+
+    }
+
+    private fun findManager(address: String): RecordingManager? {
+        return xsRecorders.find { recorder: RecordingManager -> recorder.address == address }
+    }
+
+    fun startRecording() {
+        if (xsRecorders.all { manager: RecordingManager -> manager.canRecord }) {
+            xsRecorders.forEach { manager: RecordingManager -> manager.manager.startRecording() }
+        } else {
+            activity.runOnUiThread {
+                Toast.makeText(
+                    context,
+                    "Deleting files on overcrowded Dots",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            xsRecorders.filter { manager: RecordingManager -> !manager.canRecord }
+                .forEach { it.manager.eraseRecordingData() }
         }
     }
 
-    override fun onXsensDotEraseDone(address: String?, isSuccess: Boolean) {
-        callback.notifyEraseDone(address, isSuccess)
+    fun stopRecording() {
+        if (xsRecorders.all { manager: RecordingManager -> manager.isRecording() }) {
+            xsRecorders.forEach { manager: RecordingManager -> manager.stopRecording() }
+        }
     }
 
-    override fun onXsensDotRequestFlashInfoDone(address: String?, usedFlashSpace: Int, totalFlashSpace: Int) {
-        Log.d("Request flash info done with: ",
-            "usedFlashSpace: $usedFlashSpace and totalFlashSpace:  $totalFlashSpace")
-        callback.canStartRecording(address, (1 - (totalFlashSpace / usedFlashSpace) > 0.10))
+    private fun createLoggers() {
+        for (device in devices) {
+            xsRecorders.add(
+                RecordingManager(
+                    XsensDotRecordingManager(context, device, this),
+                    device.address,
+                    label = "",
+                    person = "",
+                    logger = null,
+                    recordingAck = false,
+                    canRecord = false,
+                    dataFormat = null,
+                    ArrayList()
+                )
+            )
+        }
+    }
+
+    private fun enableNotifications() {
+        for (recorder in xsRecorders) {
+            recorder.manager.enableDataRecordingNotification()
+        }
+    }
+
+    override fun onXsensDotRecordingNotification(address: String, isEnabled: Boolean) {
+        if (isEnabled) {
+            findManager(address)?.manager?.requestFlashInfo()
+        }
+    }
+
+    override fun onXsensDotRequestFlashInfoDone(
+        address: String?,
+        usedFlashSpace: Int,
+        totalFlashSpace: Int
+    ) {
+        if (1 - usedFlashSpace / totalFlashSpace > 0.10) {
+            address?.let {
+                findManager(it)?.canRecord =
+                    true
+            }
+        }
     }
 
     override fun onXsensDotRecordingAck(
         address: String?,
         recordingId: Int,
         isSuccess: Boolean,
-        XSensDotRecordingState: XsensDotRecordingState?
+        recordingState: XsensDotRecordingState?
     ) {
-        // can get other recording states here
-        if (recordingId ==
-            XsensDotRecordingManager.RECORDING_ID_START_RECORDING) {
-            // start recording result, check recordingState, it should be success or fail.
-            callback.recordingStarted(
-                address,
-                recordingId,
-                isSuccess,
-                XSensDotRecordingState
-            )
-        } else if (recordingId ==
-            XsensDotRecordingManager.RECORDING_ID_STOP_RECORDING) {
-            // stop recording result, check recordingState, it should be success or fail.
-            callback.recordingStopped(
-                address,
-                recordingId,
-                isSuccess,
-                XSensDotRecordingState
-            )
+        if (recordingId == XsensDotRecordingManager.RECORDING_ID_START_RECORDING) {
+            // start recording result, check recordingState, it should be success or fail. ... }
+            if (recordingState == XsensDotRecordingState.success) {
+                TODO()
+            }
+            if (recordingState == XsensDotRecordingState.fail) {
+                TODO()
+            }
+        }
+        if (recordingId == XsensDotRecordingManager.RECORDING_ID_STOP_RECORDING) {
+            // stop recording result, check recordingState, it should be success or fail. ... }
+            TODO()
         }
     }
 
-    override fun onXsensDotGetRecordingTime(p0: String?, p1: Int, p2: Int, p3: Int) {
-        TODO("Not yet implemented")
+
+    override fun onXsensDotGetRecordingTime(
+        address: String?,
+        startUTCSeconds: Int,
+        totalRecordingSeconds: Int,
+        remainingRecordingSeconds: Int
+    ) {
     }
+
+    private fun requestFileInfo() {
+        xsRecorders.forEach { manager: RecordingManager -> manager.requestFileInfo() }
+    }
+
+    private fun selectStdExportQuantities() {
+        xsRecorders.forEach { manager: RecordingManager ->
+            manager.stdDataFormat()
+            manager.dataFormat?.let { manager.selectExportedData(it) }
+        }
+    }
+
+    private fun exportOnAll() {
+        if (recordersIdle())
+        xsRecorders.forEach { manager: RecordingManager -> manager.startExporting() }
+    }
+
+    private fun recordersIdle(): Boolean {
+       return xsRecorders.all { manager: RecordingManager -> manager.isIdle() }
+    }
+
+    fun startExporting() {
+        requestFileInfo()
+        selectStdExportQuantities()
+        exportOnAll()
+    }
+
 
     override fun onXsensDotRequestFileInfoDone(
         address: String?,
         list: ArrayList<XsensDotRecordingFileInfo>?,
         isSuccess: Boolean
     ) {
-        if (isSuccess) callback.canExport(address, list) else callback.cantExport(address, list)
+        if (list != null && address != null) {
+            findManager(address)?.addRecordings(list)
+        } else {
+            activity.runOnUiThread {
+                Toast.makeText(
+                    context,
+                    "No recording Files Found",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
+
+    private fun findDevice(address: String): XsensDotDevice? {
+        return devices.find { device: XsensDotDevice -> device.address == address }
+    }
+
+    private fun dataExported(
+        address: String,
+        fileInfo: XsensDotRecordingFileInfo,
+        exportedData: XsensDotData
+    ) {
+        findDevice(address)?.let {
+            findManager(address)?.logExport(
+                context,
+                it,
+                fileInfo,
+                exportedData
+            )
+        }
+    }
+
 
     override fun onXsensDotDataExported(
         address: String?,
         fileInfo: XsensDotRecordingFileInfo,
         exportedData: XsensDotData?
     ) {
-        // When the export is in progress, this callback will be called,
-        // returning each exported data XsensDotData, corresponding to the selected field
-        // Data can be stored through and written to the csv file
+        if (address != null && exportedData != null) dataExported(address, fileInfo, exportedData)
     }
 
-    override fun onXsensDotDataExported(p0: String?, p1: XsensDotRecordingFileInfo?) {
-        TODO("Not yet implemented")
+    private fun fileExported(
+        address: String
+    ) {
+        val manager = findManager(address)
+        if (manager != null) {
+            manager.dropLogger()
+            manager.hasExported = true
+        }
+    }
+
+    override fun onXsensDotDataExported(address: String?, fileInfo: XsensDotRecordingFileInfo) {
+        if (address != null) fileExported(address)
+
     }
 
     override fun onXsensDotAllDataExported(address: String?) {
-        callback.stopExporting(address)
+        if (address != null) fileExported(address)
+        if (xsRecorders.all { manager: RecordingManager -> manager.hasExported }) {
+            xsRecorders.forEach { manager: RecordingManager -> manager.clear() }
+            exportingFinished()
+        }
     }
 
-    override fun onXsensDotStopExportingData(address: String?) {
-        callback.deleteRecordings(address)
+    override fun onXsensDotStopExportingData(p0: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onXsensDotEraseDone(p0: String?, isSuccess: Boolean) {
+        if (isSuccess) activity.runOnUiThread {
+            Toast.makeText(
+                context,
+                "Successfully deleted some Files.",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun exportingFinished() {
+        callback.exportingFinished()
     }
 }
--- app/src/main/java/sensors_in_paradise/sonar/page2/RecordingInterface.kt
+++ app/src/main/java/sensors_in_paradise/sonar/page2/RecordingInterface.kt
@@ -1,33 +1,8 @@
 package sensors_in_paradise.sonar.page2
 
-import com.xsens.dot.android.sdk.models.XsensDotRecordingFileInfo
-import com.xsens.dot.android.sdk.models.XsensDotRecordingState
-
 interface RecordingInterface {
-    fun requestFlashInfo() {
-    }
-    fun canStartRecording(address: String?, b: Boolean) {
-    }
-    fun recordingStarted(
-        address: String?,
-        recordingId: Int,
-        success: Boolean,
-        xSensDotRecordingState: XsensDotRecordingState?
-    ) {
-    }
-    fun recordingStopped(
-        address: String?,
-        recordingId: Int,
-        success: Boolean,
-        xSensDotRecordingState: XsensDotRecordingState?
-    ) {
-    }
-    fun canExport(address: String?, list: ArrayList<XsensDotRecordingFileInfo>?) {
-    }
-
-    fun cantExport(address: String?, list: ArrayList<XsensDotRecordingFileInfo>?) {
-    }
-    fun stopExporting(address: String?)
-    fun deleteRecordings(address: String?)
-    fun notifyEraseDone(address: String?, isSuccess: Boolean)
+    fun startRecording()
+    fun stopRecording()
+    fun startExporting()
+    fun exportingFinished()
 }
