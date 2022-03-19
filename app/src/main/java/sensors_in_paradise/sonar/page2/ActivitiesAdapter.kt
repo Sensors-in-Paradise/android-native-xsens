2c72be53bf9afab2905e87691adeb4266fab371d
Kirill Postnov <kirill.postnov@student.hpi.uni-potsdam.de>
2022-03-19 21:57:23 +0100

    Make activities editable

--- app/src/main/java/sensors_in_paradise/sonar/page2/ActivitiesAdapter.kt
+++ app/src/main/java/sensors_in_paradise/sonar/page2/ActivitiesAdapter.kt
@@ -3,7 +3,9 @@ package sensors_in_paradise.sonar.page2
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
 import sensors_in_paradise.sonar.GlobalValues
 import sensors_in_paradise.sonar.R
@@ -12,7 +14,7 @@ class ActivitiesAdapter(private val activities: java.util.ArrayList<Pair<Long, S
     RecyclerView.Adapter<ActivitiesAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val activityTV: TextView = view.findViewById(R.id.tv_activity_recordingActivityItem)
+        val activityEditText: EditText = view.findViewById(R.id.edit_activity_recordingActivityItem)
         val startTimeTV: TextView = view.findViewById(R.id.tv_startTime_recordingActivityItem)
     }
 
@@ -25,8 +27,13 @@ class ActivitiesAdapter(private val activities: java.util.ArrayList<Pair<Long, S
 
     override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
         val activity = activities[position]
-        viewHolder.activityTV.text = activity.second
+        viewHolder.activityEditText.setText(activity.second)
         viewHolder.startTimeTV.text = getStartTimeAsString(activity)
+
+        viewHolder.activityEditText.addTextChangedListener { text ->
+            activities[position] = Pair(activity.first, text.toString())
+            notifyItemChanged(position)
+        }
     }
 
     override fun getItemCount() = activities.size
