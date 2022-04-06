d9197ba4fc2d7fec31a763ad13294619b213e90f
Alexander <grepolisiert@hotmail.de>
2022-04-06 17:19:01 +0200

    partly import poseEstimation github repo

---
+++ app/src/main/java/sensors_in_paradise/sonar/poseEstimation/CameraProcessing.kt
@@ -0,0 +1,83 @@
+// TODO: Has to be merged with existing camera implementation
+
+package sensors_in_paradise.sonar.poseEstimation
+
+
+import android.graphics.Bitmap
+import android.graphics.Rect
+import android.view.SurfaceView
+import sensors_in_paradise.sonar.poseEstimation.data.Person
+
+class CameraProcessing(private val surfaceView: SurfaceView)
+// private val listener: CameraSourceListener? = null
+{
+    companion object {
+        /** Threshold for confidence score. */
+        private const val MIN_CONFIDENCE = .2f
+        private const val TAG = "Camera Source"
+    }
+
+    private val lock = Any()
+    private var detector: PoseDetector? = null
+    private val isTrackerEnabled = false
+
+    private var frameProcessedInOneSecondInterval = 0
+    private var framesPerSecond = 0
+
+    // process image
+    private fun processImage(bitmap: Bitmap) {
+        val persons = mutableListOf<Person>()
+
+        synchronized(lock) {
+            detector?.estimatePoses(bitmap)?.let {
+                persons.addAll(it)
+            }
+        }
+        // frameProcessedInOneSecondInterval++
+        // if (frameProcessedInOneSecondInterval == 1) {
+        //    // send fps to view
+        //    listener?.onFPSListener(framesPerSecond)
+        // }
+
+        visualize(persons, bitmap)
+    }
+
+    private fun visualize(persons: List<Person>, bitmap: Bitmap) {
+
+        val outputBitmap = VisualizationUtils.drawBodyKeypoints(
+            bitmap,
+            persons.filter { it.score > MIN_CONFIDENCE }, isTrackerEnabled
+        )
+
+        val holder = surfaceView.holder
+        val surfaceCanvas = holder.lockCanvas()
+        surfaceCanvas?.let { canvas ->
+            val screenWidth: Int
+            val screenHeight: Int
+            val left: Int
+            val top: Int
+
+            if (canvas.height > canvas.width) {
+                val ratio = outputBitmap.height.toFloat() / outputBitmap.width
+                screenWidth = canvas.width
+                left = 0
+                screenHeight = (canvas.width * ratio).toInt()
+                top = (canvas.height - screenHeight) / 2
+            } else {
+                val ratio = outputBitmap.width.toFloat() / outputBitmap.height
+                screenHeight = canvas.height
+                top = 0
+                screenWidth = (canvas.height * ratio).toInt()
+                left = (canvas.width - screenWidth) / 2
+            }
+            val right: Int = left + screenWidth
+            val bottom: Int = top + screenHeight
+
+            canvas.drawBitmap(
+                outputBitmap, Rect(0, 0, outputBitmap.width, outputBitmap.height),
+                Rect(left, top, right, bottom), null
+            )
+            surfaceView.holder.unlockCanvasAndPost(canvas)
+        }
+    }
+}
