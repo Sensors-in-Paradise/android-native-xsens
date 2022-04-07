32af006a55397ff5f7897144304242d82b00cb98
tfiedlerdev <tfiedlerdevelopment@gmail.com>
2022-04-07 16:29:34 +0200

    Satisfy Chefe!

--- app/src/main/java/sensors_in_paradise/sonar/custom_views/stickman/object3d/Sensor3D.kt
+++ app/src/main/java/sensors_in_paradise/sonar/custom_views/stickman/object3d/Sensor3D.kt
@@ -14,6 +14,10 @@ class Sensor3D : Cube() {
                 Color.valueOf(253f / 255f, 109f / 255f, 83f / 255f, 0.5f).toArgb()
             )
         )
+        translate(0f, -0.5f, 0f, false)
+        scale(1.25f, 0.25f, 0.75f, false)
+        updateDefaultState()
+
         children.add(CoordinateSystem3D())
     }
 }
