8e40e701fc5134f524a9196acc1e811993ee510e
alexanderwischmann <57876251+alexanderwischmann@users.noreply.github.com>
2022-04-26 15:03:39 +0200

    Update ClickableCarouselTextView.kt

--- app/src/main/java/sensors_in_paradise/sonar/page2/labels_editor/ClickableCarouselTextView.kt
+++ app/src/main/java/sensors_in_paradise/sonar/page2/labels_editor/ClickableCarouselTextView.kt
@@ -6,7 +6,7 @@ import android.util.AttributeSet
 class ClickableCarouselTextView(context: Context, attrSet: AttributeSet) :
     androidx.appcompat.widget.AppCompatTextView(context, attrSet) {
     private var onClickListener: OnClickListener? = null
-        
+
     override fun setOnClickListener(listener: OnClickListener?) {
         onClickListener = listener
     }
