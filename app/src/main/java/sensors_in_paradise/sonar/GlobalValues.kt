32fcde285a353b6c2ff8c57a35eddbcaa4ca05c1
Lucas Liebe <lucasliebe.ll@gmail.com>
2022-05-31 12:27:48 +0200

    add newlines

--- app/src/main/java/sensors_in_paradise/sonar/GlobalValues.kt
+++ app/src/main/java/sensors_in_paradise/sonar/GlobalValues.kt
@@ -16,6 +16,7 @@ class GlobalValues private constructor() {
         const val ACTIVE_RECORDING_FLAG_FILENAME = "active"
         const val METADATA_JSON_FILENAME = "metadata.json"
         const val MEASUREMENT_MODE = XsensDotPayload.PAYLOAD_TYPE_CUSTOM_MODE_4
+
         fun getSensorRecordingsBaseDir(context: Context): File {
             return context.getExternalFilesDir(null) ?: context.dataDir
         }
@@ -23,9 +24,11 @@ class GlobalValues private constructor() {
         fun getSensorRecordingsTempDir(context: Context): File {
             return context.dataDir.resolve("temp")
         }
+
         fun getVideoRecordingsTempDir(context: Context): File {
             return context.dataDir.resolve("videoTemp")
         }
+
         fun getPoseRecordingsTempDir(context: Context): File {
             return context.dataDir.resolve("poseTemp")
         }
@@ -54,6 +57,7 @@ class GlobalValues private constructor() {
         fun formatTag(tagPrefix: String, deviceSetKey: String): String {
             return "$tagPrefix-$deviceSetKey"
         }
+
         fun getDurationAsString(durationMS: Long): String {
 
             val diffSecs = (durationMS) / 1000
@@ -67,6 +71,7 @@ class GlobalValues private constructor() {
             "json" to "\uD83D\uDCD8",
             "csv" to "\uD83D\uDCCA"
         )
+
         fun getFileEmoji(file: File): String {
             if (file.isDirectory) {
                 return "\uD83D\uDCC1"
