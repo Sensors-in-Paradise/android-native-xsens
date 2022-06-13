7b19b8739c17990908c6c869ee1203ad3e53ad8d
tfiedlerdev <tfiedlerdevelopment@gmail.com>
2022-06-13 14:15:21 +0200

    Replace default model

--- app/src/main/java/sensors_in_paradise/sonar/use_cases/UseCase.kt
+++ app/src/main/java/sensors_in_paradise/sonar/use_cases/UseCase.kt
@@ -76,7 +76,7 @@ class UseCase(
     }
 
     fun importDefaultModel() {
-        extractFileFromAssets(context, "resnet_model.tflite", getModelFile())
+        extractFileFromAssets(context, "model.tflite", getModelFile())
     }
 
     fun getDisplayInfo(): String {
